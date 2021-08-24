9805d784d4b5282f7da30649d870d4e6babf1396
ehdtlaos <eee910413@gmail.com>
2021-08-24 14:48:18 -0700

    completed retrieving reply data and displaying it on the screen

--- client/dist/index.bundle.js
+++ client/dist/index.bundle.js
@@ -2090,18 +2090,103 @@ function Posting() {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
-/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _Reply_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Reply.css */ "./src/components/Reply.css");
-/* harmony import */ var _Reply_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_Reply_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/asyncToGenerator */ "./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js");
+/* harmony import */ var _babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/slicedToArray */ "./node_modules/@babel/runtime/helpers/esm/slicedToArray.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _Reply_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./Reply.css */ "./src/components/Reply.css");
+/* harmony import */ var _Reply_css__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_Reply_css__WEBPACK_IMPORTED_MODULE_5__);
+
+
+
 
 
 
 
 function Reply(_ref) {
   var id = _ref.id;
-  return null;
+
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_3__.useState)(null),
+      _useState2 = (0,_babel_runtime_helpers_slicedToArray__WEBPACK_IMPORTED_MODULE_1__.default)(_useState, 2),
+      currentReply = _useState2[0],
+      setCurrentReply = _useState2[1];
+
+  console.log(id);
+
+  var getReply = /*#__PURE__*/function () {
+    var _ref2 = (0,_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_0__.default)( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2___default().mark(function _callee(comment_id) {
+      var fetchReply;
+      return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_2___default().wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.prev = 0;
+              _context.next = 3;
+              return axios__WEBPACK_IMPORTED_MODULE_4___default().get("/posting/".concat(comment_id, "/reply"));
+
+            case 3:
+              fetchReply = _context.sent;
+              console.log(fetchReply);
+
+              if (fetchReply.data !== undefined) {
+                setCurrentReply(fetchReply.data);
+              } else {
+                setCurrentReply('none');
+              }
+
+              _context.next = 11;
+              break;
+
+            case 8:
+              _context.prev = 8;
+              _context.t0 = _context["catch"](0);
+              console.log('error when fetching reply data');
+
+            case 11:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee, null, [[0, 8]]);
+    }));
+
+    return function getReply(_x) {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
+  (0,react__WEBPACK_IMPORTED_MODULE_3__.useEffect)(function () {
+    getReply(id);
+  }, []);
+  console.log(currentReply);
+
+  if (currentReply !== null && currentReply !== 'none') {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3__.createElement("div", {
+      className: "reply_main"
+    }, currentReply.map(function (reply) {
+      var _reply$reply_created, _reply$reply_like, _reply$reply_dislike;
+
+      return reply.comment_id === id ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3__.createElement("div", {
+        className: "replies",
+        key: reply.reply_id
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3__.createElement("div", {
+        className: "reply_name"
+      }, reply.reply_nick_name), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3__.createElement("div", {
+        className: "reply_date"
+      }, (_reply$reply_created = reply.reply_created) !== null && _reply$reply_created !== void 0 ? _reply$reply_created : 'Aug 24, 2021'), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3__.createElement("div", {
+        className: "reply_body"
+      }, reply.reply_body), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3__.createElement("div", {
+        className: "reply_like"
+      }, (_reply$reply_like = reply.reply_like) !== null && _reply$reply_like !== void 0 ? _reply$reply_like : 0), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_3__.createElement("div", {
+        className: "reply_dislike"
+      }, (_reply$reply_dislike = reply.reply_dislike) !== null && _reply$reply_dislike !== void 0 ? _reply$reply_dislike : 0)) : null;
+    }));
+  } else {
+    return null;
+  }
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (Reply);
--- client/src/components/Reply.jsx
+++ client/src/components/Reply.jsx
@@ -3,9 +3,47 @@ import axios from 'axios';
 import './Reply.css';
 
 function Reply({id}) {
+  const [currentReply, setCurrentReply] = useState(null);
+  console.log(id);
+  const getReply = async (comment_id) => {
+    try {
+      const fetchReply = await axios.get(`/posting/${comment_id}/reply`);
+      console.log(fetchReply);
+      if (fetchReply.data !== undefined) {
+        setCurrentReply(fetchReply.data);
+      } else{
+        setCurrentReply('none');
+      }
+    } catch(e) {
+      console.log('error when fetching reply data');
+    }
+  }
 
+  useEffect(() => {
+    getReply(id)
+  }, [])
 
-  return null;
+  console.log(currentReply);
+
+  if (currentReply !== null && currentReply !== 'none') {
+    return (
+      <div className="reply_main">
+        {currentReply.map((reply) => (
+          (reply.comment_id === id) ? (
+          <div className="replies" key={reply.reply_id}>
+            <div className="reply_name">{reply.reply_nick_name}</div>
+            <div className="reply_date">{reply.reply_created ?? 'Aug 24, 2021'}</div>
+            <div className="reply_body">{reply.reply_body}</div>
+            <div className="reply_like">{reply.reply_like ?? 0}</div>
+            <div className="reply_dislike">{reply.reply_dislike ?? 0}</div>
+          </div>
+          ) : null
+        ))}
+      </div>
+    )
+  } else {
+    return null;
+  }
 }
 
 export default Reply;
